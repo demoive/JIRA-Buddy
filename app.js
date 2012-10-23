13ae221489d52a47eac428c4407c24de07cda4f9
Paulo Ávila <pauloavila@gmail.com>
2012-10-23 16:34:48 +0200

    Added basic Options page & updated name
    Added an options page to allow the Atlassian account and project id to
    be configurable.
    Changed extension name to be more friendly.

--- app.js
+++ app.js
@@ -1,75 +1,101 @@
 ﻿(function () {
-    "use strict";
+	"use strict";
 
-    var JIRA_ACCOUNT_ID = "waytostay",
-        JIRA_PROJECT_PREFIX = "WTS";
+	var JIRA_ACCOUNT_ID = localStorage['JIRA_ACCOUNT_ID'],
+		JIRA_PROJECT_PREFIX = localStorage['JIRA_PROJECT_PREFIX'];
 
-    /**
-     * jQuery's onDocumentReady function: called when
-     * the DOM has been fully loaded (not including resources).
-     *
-     * Initializes the pop-up and defines the functions
-     */
-    $(function() {
-        var $searchForm = $('#search-form'),
-            $searchInput = $('#search-input'),
-            $searchSubmit = $('#search-submit'),
-            requestedIssue;
+	/**
+	 * jQuery's onDocumentReady function: called when
+	 * the DOM has been fully loaded (not including resources).
+	 *
+	 * Initializes the pop-up and defines the functions
+	 */
+	$(function() {
+		var $searchForm = $('#search-form'),
+			$searchInput = $('#search-input'),
+			$searchSubmit = $('#search-submit'),
+			requestedIssue;
 
-        $searchInput.focus();
-        $searchSubmit.value = chrome.i18n.getMessage("submitButtonLabel");
+		if (!JIRA_ACCOUNT_ID || !JIRA_PROJECT_PREFIX) {
+			$searchForm.html('<a href="options.html" target="_blank">Settings</a>');
 
+			return;
+		}
 
-        /**
-         * Called by Chrome's tab query function.
-         *
-         * @param {Array} Array of Tabs that match the query
-         */
-        function showOrOpenIssue(tabs) {
-            var i,
-                reg = new RegExp("^https?://" + JIRA_ACCOUNT_ID + ".atlassian.net/browse/" + requestedIssue);
-
-            // bring focus to the first tab that matches the regular expression for an issue
-            if ((tabs && tabs.length) > 0) {
-                for (i = 0; i < tabs.length; ++i) {
-                    if (tabs[i].url.match(reg) !== null) {
-                        chrome.tabs.update(tabs[i].id, {
-                            selected: true
-                        });
-
-                        return;
-                    }
-                }
-            }
-
-            // issue is not open in any tab, so open a new tab for it
-            chrome.tabs.create({
-                url: "https://" + JIRA_ACCOUNT_ID + ".atlassian.net/browse/" + requestedIssue
-            });
-        }
+		$searchInput.attr('value', ((localStorage['lastRequest']) || ''));
+		
+		$searchInput.focus();
+		$searchInput.select();
+		$searchSubmit.value = chrome.i18n.getMessage("submitButtonLabel");
 
 
-        /**
-         * Called when the search form is submitted.
-         *
-         * If the input text doesn't include the project prefix,
-         * prepends it to the issue number before checking currently open tabs.
-         */
-        function submitted() {
-            var hasPrefixRegExp = /.+-\d+/;
+		/**
+		 * Called by Chrome's tab query function.
+		 *
+		 * @param {Array} Array of Tabs that match the query
+		 */
+		function showOrOpenIssue(tabs) {
+			var i,
+				reg = new RegExp("^https?://" + JIRA_ACCOUNT_ID + ".atlassian.net/browse/" + requestedIssue);
 
-            requestedIssue = $searchInput.attr('value');
+			// bring focus to the first tab that matches the regular expression for an issue
+			if ((tabs && tabs.length) > 0) {
+				for (i = 0; i < tabs.length; ++i) {
+					if (tabs[i].url.match(reg) !== null) {
+						chrome.tabs.update(tabs[i].id, {
+							selected: true
+						});
 
-            //issue = isNaN(issue) ? issue : (JIRA_PROJECT_PREFIX + "-" + issue);
-            requestedIssue = hasPrefixRegExp.test(requestedIssue) ? requestedIssue : (JIRA_PROJECT_PREFIX + "-" + requestedIssue);
+						return;
+					}
+				}
+			}
 
-            chrome.tabs.query({
-                url: "https://" + JIRA_ACCOUNT_ID + ".atlassian.net/*"
-            }, showOrOpenIssue);
+			// issue is not open in any tab, so open a new tab for it
+			chrome.tabs.create({
+				url: "https://" + JIRA_ACCOUNT_ID + ".atlassian.net/browse/" + requestedIssue
+			});
+		}
 
-            return false;
-        }
 
-        $searchForm.on('submit', submitted);
-    });
+		/**
+		 * Called when the search form is submitted.
+		 *
+		 * If the input text doesn't include the project prefix,
+		 * prepends it to the issue number before checking currently open tabs.
+		 */
+		function submitted() {
+			var hasPrefixRegExp = /.+-\d+/;
+
+			requestedIssue = $searchInput.attr('value');
+
+			//issue = isNaN(issue) ? issue : (JIRA_PROJECT_PREFIX + "-" + issue);
+			requestedIssue = hasPrefixRegExp.test(requestedIssue) ? requestedIssue : (JIRA_PROJECT_PREFIX + "-" + requestedIssue);
+
+			chrome.tabs.query({
+				url: "https://" + JIRA_ACCOUNT_ID + ".atlassian.net/*"
+			}, showOrOpenIssue);
+
+			localStorage['lastRequest'] = requestedIssue;
+
+			return false;
+		}
+
+		$searchForm.on('submit', submitted);
+	});
 }());
+
+
+/*
+Popup:
+- goto form
+- recent searches
+- set banners
+
+Background:
+- check every minute for issues assigned to me
+- ! if can't check
+
+User script
+- inject thing into jira pages
+*/
